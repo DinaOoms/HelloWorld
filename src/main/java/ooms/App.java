bf82a41978eb4fd47aa53de7019b79382c3a28c9
DinaOoms <dina.ooms.antwerpen@gmail.com>
2018-06-02 10:18:37 +0200

    start learning java

---
+++ src/main/java/ooms/App.java
@@ -0,0 +1,19 @@
+package ooms;
+
+import java.time.LocalDate;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "privet Dina!" );
+        LocalDate today = LocalDate.now();
+        System.out.println(today.getYear());
+
+
+    }
+}
